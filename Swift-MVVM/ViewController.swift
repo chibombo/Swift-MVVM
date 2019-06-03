31c2f5ce30b9d53902380d1a77fa6f9e7bb05616
chibombo <genaro.arvizu95@gmail.com>
2019-06-02 22:11:25 -0500

    base project

---
+++ Swift-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-MVVM
+//
+//  Created by Luis Genaro Arvizu Vega on 6/2/19.
+//  Copyright © 2019 Luis Genaro Arvizu Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
